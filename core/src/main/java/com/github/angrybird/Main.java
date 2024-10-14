cd48f51a4a2244f4a68c7ccf2baf32aa64ce3093
arrpt <arpitrajput5605@gmai.com>
2024-10-14 17:37:54 +0530

    fitviewport, no stretched-res

--- core/src/main/java/com/github/angrybird/Main.java
+++ core/src/main/java/com/github/angrybird/Main.java
@@ -2,28 +2,37 @@ package com.github.angrybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private FitViewport viewport;
 
     @Override
     public void create() {
+        viewport = new FitViewport(640, 480);
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(Color.BLACK);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override

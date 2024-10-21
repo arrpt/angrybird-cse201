2fd8f86f7670e0a5d813dcd3fbdb8999f6e5b202
RuSKie147 <raj23422@iiitd.ac.in>
2024-10-22 01:26:59 +0530

    mouse hover
    added mouse hover on play button

--- core/src/main/java/com/github/angrybird/StartupScreen.java
+++ core/src/main/java/com/github/angrybird/StartupScreen.java
@@ -13,6 +13,8 @@ public class StartupScreen implements Screen {
     Main game;
     private Texture bgimage;
     private Music bgmusic;
+    private Texture playButton1;
+    private Texture playButton2;
 
     public StartupScreen(Main game){
         this.game = game;
@@ -22,6 +24,8 @@ public class StartupScreen implements Screen {
     public void show() {
         bgmusic = Gdx.audio.newMusic(Gdx.files.internal("intro2.mp3"));
         bgimage = new Texture("angrybirdsintro3.png"); // credits - https://www.deviantart.com/krimadraws/art/Angry-Birds-Splash-screen-but-even-more-Classic-916194469
+        playButton1 = new Texture("play6.png");
+        playButton2 = new Texture("play7.png");
     }
 
     @Override
@@ -29,7 +33,18 @@ public class StartupScreen implements Screen {
         bgmusic.play();
         ScreenUtils.clear(Color.BLACK);
         game.batch.begin();
-        game.batch.draw(bgimage, 0, 0, 1280, 720);
+        game.batch.draw(bgimage, 0, 0, 1280f, 720f);
+
+        game.batch.draw(playButton1, 640f-(100f/2f), 60f, 100f, 97.67f);
+        //change to playButton2 if hover
+        if(Gdx.input.getX()>590f && Gdx.input.getX()<690f && 720f-Gdx.input.getY()>60f && 720f-Gdx.input.getY()<157.67f){
+            game.batch.draw(playButton2, 640f-(100f/2f), 60f, 100f, 97.67f);
+            if(Gdx.input.isTouched()){
+
+                //game.setScreen(new GameScreen(game));
+            }
+        }
+
         game.batch.end();
     }
 

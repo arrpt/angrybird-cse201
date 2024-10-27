8d76ca0b87af3ebb33a8228aff4ddf7530694a38
arrpt <arpitrajput5605@gmai.com>
2024-10-27 21:54:06 +0530

    added suar

--- core/src/main/java/com/github/angrybird/KingPig.java
+++ core/src/main/java/com/github/angrybird/KingPig.java
@@ -3,14 +3,16 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-public class RedBird {
+public class KingPig {
     private BodyDef body;
     private FixtureDef fixture;
     private Sprite sprite;
+    private int health;
 
-    public RedBird(){
+    public KingPig(){
         body = new BodyDef();
         body.type = BodyDef.BodyType.DynamicBody;
         fixture = new FixtureDef();
+        health = 300;
     }
 }
